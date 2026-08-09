523239d54328007ab56cb50015ff0a14ab5657ea
Sanskar <sanskarin@outlook.in>
2026-08-09 12:28:23 +0530

    build: make MAUI and DI namespaces explicit

--- src/CareNest.App/GlobalUsings.cs
+++ src/CareNest.App/GlobalUsings.cs
@@ -1,1 +1,5 @@
 global using CareNest.Application.Services;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Maui;
+global using Microsoft.Maui.Controls;
+global using Microsoft.Maui.Hosting;
